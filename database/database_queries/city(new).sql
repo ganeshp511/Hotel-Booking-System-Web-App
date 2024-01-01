dedd0c1a47e8ee220bed68fea8083fa99687c92e
Ganesh Phad <phadganesh.com>
2024-01-01 18:57:58 +0530

    updated project files

--- database/database_queries/city(new).sql
+++
@@ -1,5 +0,0 @@
-create table city(city_id int not null, city_name char(20), primary key(city_id));
-
-insert into city values(1, 'Pune');
-insert into city values(2, 'Mumbai'),(3, 'Chennai'),(4, 'Hyderabad'), 
-(5, 'Bangalore'),(6, 'Delhi'),(7, 'Jaipur'),(8, 'Kolkata'),(9, 'Bhopal'),(10, 'Amritsar');
